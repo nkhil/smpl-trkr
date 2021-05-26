c1676a26638b9edf733d294dfaa9f4470e8bcb8c
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-05-26 23:42:17 +0100

    chore: add logger

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./src');
+const logger = require('@subgeniuscorp/logger');
+const { PORT } = require('./src/config');
 
 app.listen(8080, () => {
-  console.log(`Server running on port 8080`);
+  logger.info(`Server running on port ${PORT}`);
 });
