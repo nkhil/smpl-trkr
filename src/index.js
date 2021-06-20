2b63cba2edf6a1018c895ea65739a4aa9c280bef
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2021-06-20 01:24:31 +0100

    feat: add metric validator

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-
-const extractDetailsFromRequest = require('./helpers/extract-details');
+const { metricsEmitter } = require('./controller/metrics');
 
 app.use(cookieParser());
 app.use(cors());
@@ -11,21 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('static'));
 
-let result = {}
-
-app.use('/t/:id/:name', (req, res) => {
-  const { id, name } = req.params
-  const { host, ipAddress, userAgent } = extractDetailsFromRequest(req);
-  if (result[id] && result[id][name]) {
-    result[id][name] = result[id][name] += 1
-  } else {
-    if (result[id]) {
-      result[id][name] = 1
-    } else {
-      result[id] = { [name]: 1 }
-    }
-  }
+app.use('/t/:userId/:sourceId', (req, res) => {
+  const { userId, sourceId } = req.params
   res.status(204).end()
+  metricsEmitter({ userId, sourceId })
 });
 
 module.exports = app;
